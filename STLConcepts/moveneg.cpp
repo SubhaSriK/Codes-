ca187ab62e1642840177fbacdcc17594f5a884e2
Subha Sri K <99090748+SubhaSriK@users.noreply.github.com>
2025-01-05 21:16:57 +0530

    Create moveneg.cpp

---
+++ STLConcepts/moveneg.cpp
@@ -0,0 +1,21 @@
+  void segregateElements(vector<int>& arr) {
+        // 
+        vector<int> pos , neg;
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]<0){
+                neg.push_back(arr[i]);
+            }
+             else{
+                 pos.push_back(arr[i]);
+             }
+        }
+        int j=0;
+        for(int i=0;i<pos.size();i++){
+            arr[j] = pos[i];
+            j++;
+        }
+        for(int i=0;i<neg.size();i++){
+            arr[j] = neg[i];
+            j++;
+        }
+    }
